313ed5f9b6bd41a951c2ac2ad6f64397c2420655
Daniel Melville <danielmelville@mmc.school.nz>
2020-08-18 11:33:31 +1200

    Completed main layout.

---
+++ TheCardCove/Cards.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace TheCardCove
+{
+    class Cards
+    {
+        // declare fields to use in the class
+        public int x, y, width, height;//variables for the cards
+        public Image card;//variable for the card's image
+        public Rectangle CardsRec;//variable for a rectangle to place our image in
+        public int score;
+        //Create a constructor (initialises the values of the fields)
+        public Cards()
+        {
+            x = 10;
+            y = 10;
+            width = 20;
+            height = 20;
+
+            //planetImage contains the plane1.png image
+            card = Image.FromFile("card.png");
+            CardsRec = new Rectangle(x, y, width, height);
+        }
+
+        // Methods for the Cards class
+        public void DrawCards(Graphics g)
+        {
+            g.DrawImage(card, CardsRec);
+        }
+    }
+}
--- TheCardCove/Form1.cs
+++ TheCardCove/Form1.cs
@@ -12,6 +12,10 @@ namespace TheCardCove
 {
     public partial class Form1 : Form
     {
+
+        Graphics g; //declare a graphics object called g
+        Cards card = new Cards(); //create the object, planet1
+
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +29,14 @@ namespace TheCardCove
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            //get the graphics used to paint on the panel control
+            g = e.Graphics;
+            //call the Planet class&#39;s DrawPlanet method to draw the image cards
+            card.DrawCards(g);
         }
     }
 }
